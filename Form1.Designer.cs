0136f99656613bacb9e4b2044d0c060efc107df7
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2024-04-12 22:14:18 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace BancoThinkLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtNumeroCredito = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCuota = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblNumeroCredito = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCuotaMensual = new System.Windows.Forms.Label();
+            this.lblMontoFinal = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(552, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AREA DE CREDITOS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtCuota);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtMonto);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtSueldo);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNumeroCredito);
+            this.groupBox1.Location = new System.Drawing.Point(12, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 231);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de Ingreso";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.groupBox2.Controls.Add(this.lblMontoFinal);
+            this.groupBox2.Controls.Add(this.lblCuotaMensual);
+            this.groupBox2.Controls.Add(this.lblNombre);
+            this.groupBox2.Controls.Add(this.lblNumeroCredito);
+            this.groupBox2.Location = new System.Drawing.Point(318, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 231);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultado del Credito";
+            // 
+            // txtNumeroCredito
+            // 
+            this.txtNumeroCredito.Location = new System.Drawing.Point(144, 37);
+            this.txtNumeroCredito.Name = "txtNumeroCredito";
+            this.txtNumeroCredito.Size = new System.Drawing.Size(116, 20);
+            this.txtNumeroCredito.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero de Credito";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(144, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(116, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre Completo";
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(144, 91);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(116, 20);
+            this.txtSueldo.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Sueldo";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(144, 118);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(116, 20);
+            this.txtMonto.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Monto Solicitado";
+            // 
+            // txtCuota
+            // 
+            this.txtCuota.Location = new System.Drawing.Point(144, 145);
+            this.txtCuota.Name = "txtCuota";
+            this.txtCuota.Size = new System.Drawing.Size(116, 20);
+            this.txtCuota.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Cantidad de Cuotas";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCalcular.Location = new System.Drawing.Point(63, 184);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular ";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblNumeroCredito
+            // 
+            this.lblNumeroCredito.AutoSize = true;
+            this.lblNumeroCredito.Location = new System.Drawing.Point(64, 44);
+            this.lblNumeroCredito.Name = "lblNumeroCredito";
+            this.lblNumeroCredito.Size = new System.Drawing.Size(95, 13);
+            this.lblNumeroCredito.TabIndex = 0;
+            this.lblNumeroCredito.Text = "Numero de Credito";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(64, 71);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(113, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre del Solicitante";
+            // 
+            // lblCuotaMensual
+            // 
+            this.lblCuotaMensual.AutoSize = true;
+            this.lblCuotaMensual.Location = new System.Drawing.Point(64, 98);
+            this.lblCuotaMensual.Name = "lblCuotaMensual";
+            this.lblCuotaMensual.Size = new System.Drawing.Size(78, 13);
+            this.lblCuotaMensual.TabIndex = 2;
+            this.lblCuotaMensual.Text = "Cuota Mensual";
+            // 
+            // lblMontoFinal
+            // 
+            this.lblMontoFinal.AutoSize = true;
+            this.lblMontoFinal.Location = new System.Drawing.Point(64, 125);
+            this.lblMontoFinal.Name = "lblMontoFinal";
+            this.lblMontoFinal.Size = new System.Drawing.Size(125, 13);
+            this.lblMontoFinal.TabIndex = 3;
+            this.lblMontoFinal.Text = "Monto al Final del credito";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 311);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(600, 350);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Banco Think";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumeroCredito;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCuota;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblNumeroCredito;
+        private System.Windows.Forms.Label lblMontoFinal;
+        private System.Windows.Forms.Label lblCuotaMensual;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BancoThinkLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            txtNumeroCredito.BackColor = Color.White;
+            txtNombre.BackColor = Color.White;
+            txtSueldo.BackColor = Color.White;
+            string numeroCredito = txtNumeroCredito.Text.Trim();
+            try
+            {
+                int numeroCredito_int = Convert.ToInt32(numeroCredito);
+
+                if (numeroCredito.Length == 0)
+                {
+                    MessageBox.Show("Cuadro esta vacio");
+                    return;
+                }
+            }
+            catch(FormatException ex)
+            {
+                txtNumeroCredito.BackColor = Color.Red;
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+
+
+
+            string nombre = txtNombre.Text.Trim();
+            if (nombre.Length == 0)
+            {
+                txtNombre.BackColor = Color.Red;
+                MessageBox.Show("Debe ingresar un Nombre");
+                return;
+            }
+
+
+            string sueldo = txtSueldo.Text.Trim();
+            double sueldo_dou = 0;
+            double maximo_solicitud = 0;
+            try
+            {
+                sueldo_dou = Convert.ToDouble(sueldo);
+                maximo_solicitud = sueldo_dou * 150 / 100;
+
+                MessageBox.Show(maximo_solicitud.ToString());
+            }
+            catch(FormatException ex) {
+                txtSueldo.BackColor = Color.Red;
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            string monto = txtMonto.Text.Trim();
+            double monto_dou = 0;
+            try
+            {
+                monto_dou = Convert.ToDouble(monto);
+                if( monto_dou > maximo_solicitud )
+                {
+                    MessageBox.Show("Monto solicitado excede el maximo de 150% del sueldo");
+                    return;
+                }
+                
+            }catch (FormatException ex)
+            {
+                txtMonto.BackColor = Color.Red;
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            string cuota = txtCuota.Text.Trim();
+            int cuota_int = 0;
+            try
+            {
+                cuota_int = Convert.ToInt32(cuota);
+                if(cuota_int < 6)
+                {
+                    txtCuota.BackColor = Color.Red;
+                    MessageBox.Show("Cantidad de cuotas no puede ser menor a 6");
+                    return;
+                }
+            } catch(FormatException ex)
+            {
+                txtCuota.BackColor = Color.Red;
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+
+            lblNumeroCredito.Text = numeroCredito;
+            lblNombre.Text = nombre;
+
+
+            float interes_mensual = 1.85f / 12f;
+            MessageBox.Show(interes_mensual.ToString());
+
+            double cuota_mensual = monto_dou * interes_mensual ;
+
+            lblCuotaMensual.Text = cuota_mensual.ToString();
+
+            lblMontoFinal.Text = ((cuota_mensual * cuota_int) + monto_dou).ToString(); 
+
+            MessageBox.Show(cuota_mensual.ToString());
+        }
+    }
+}
